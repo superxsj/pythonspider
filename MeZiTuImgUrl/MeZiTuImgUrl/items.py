70fc8e88e22cbb69394af94a483f3039d8ef46ad
xusijie <1321411003@qq.com>
2018-05-14 19:51:38 +0800

    spider success

--- MeZiTuImgUrl/MeZiTuImgUrl/items.py
+++ MeZiTuImgUrl/MeZiTuImgUrl/items.py
@@ -5,10 +5,11 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item ,Field
 
 
-class MezituimgurlItem(scrapy.Item):
+class MezituimgurlItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = Field()
+    url = Field()
--- MeZiTuImgUrl/MeZiTuImgUrl/spiders/meizi.py
+++ MeZiTuImgUrl/MeZiTuImgUrl/spiders/meizi.py
@@ -1,11 +1,41 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy import Request
 
-
+quc = []
 class MeiziSpider(scrapy.Spider):
     name = 'meizi'
-    allowed_domains = ['http://www.mzitu.com']
-    start_urls = ['http://http://www.mzitu.com/']
+    allowed_domains = ['www.mzitu.com']
+    start_urls = ['http://www.mzitu.com/']
+
+
 
     def parse(self, response):
-        pass
+        urls = response.css('.postlist span a[target*=_blank]::attr(href)').extract()
+
+        for url in urls:
+            yield Request(url,callback=self.parse_img)
+        #yield Request('http://www.mzitu.com/134304', callback=self.parse_img)
+        nexts = response.css('.nav-links a[class*=next]::attr(href)').extract()
+        yield Request(nexts, callback=self.parse)
+
+def parse_img(self, response):
+        urls = response.css('.main-image img::attr(src)').extract()
+        for url in urls:
+            print(url)
+        next = ''
+        index = response.css('.pagenavi a::attr(href)').extract()
+        for ind in index:
+            next = ind
+        #print(next)
+        try:
+            yield Request(next, callback=self.parse_img)
+        except ValueError:
+            yield
+
+        #next = response.css('.pagenavi a::attr(href)').extract()
+        #yield Request(next, callback=self.parse_img)
+
+
+
+
